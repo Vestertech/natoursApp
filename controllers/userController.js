d74c499b2d5936f83867d9e5dabb4b328febbb91
Vestertech <vesteerwood@gmail.com>
2023-03-31 11:38:14 +0100

    Added Multer: for Image uploads

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,24 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    // user-76545-333334555.jpeg, user-id-file ext.
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-76545-333334555.jpeg, user-id-file ext.
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
-  if (file.mimetypestr.startsWith('image')) {
+  if (file.mimetype.startsWith('image')) {
     cb(null, true);
   } else {
     cb(new AppError('Not an Image!, Please upload only images.', 400), false);
@@ -27,8 +29,22 @@ const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter
 });
+// console.log({ upload });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -70,12 +86,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
   if (req.file) filteredBody.photo = req.file.filename;
-
+  // console.log(req.fieldname, 'req.file.filename');
   // 3)  Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
   });
+  // console.log({ filteredBody });
+
   res.status(200).json({
     status: 'success',
     data: {
