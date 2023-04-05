bbbf3af435895448850beba4c9a7d570c40bab57
Vestertech <vesteerwood@gmail.com>
2023-04-05 17:04:31 +0100

    Added advanced Emailing and Paystack Integration

---
+++ models/bookingModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+  {
+    bookingId: {
+      type: String,
+      required: [true, 'Booking must have a bookingId'],
+      unique: true
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Booking must belong to a tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a user']
+    },
+    price: {
+      type: Number,
+      required: [true, 'Booking must have a price']
+    },
+    paid: {
+      type: Boolean,
+      default: true
+    },
+    tourDate: {
+      type: Date,
+      required: [true, 'Booking must have a date']
+    },
+    paidAt: {
+      type: Date,
+      required: [true, 'Booking must have a paidAt date']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true
+  }
+);
+
+bookingSchema.pre('/^find/', function(next) {
+  this.populate({ path: 'tour', select: 'name' }).populate({
+    path: 'user',
+    select: 'name email '
+  });
+  return next();
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
